d686dc2babd542a650508a470c347b5af755f695
EffDuBois <arnabkar2003@protonmail.com>
2023-02-02 00:58:38 +0530

    Fixed props.children issue

--- Components/Elements/AppText.js
+++ Components/Elements/AppText.js
@@ -2,9 +2,7 @@ import { Text, StyleSheet } from "react-native";
 
 export default function AppText(props) {
   return (
-    <AppText>
       <Text style={[styles.text, props.style]}>{props.children}</Text>
-    </AppText>
   );
 }
 
--- Components/StyleComponents/styles.js
+++ Components/StyleComponents/styles.js
@@ -20,5 +20,4 @@ export default StyleSheet.create({
     alignSelf: "stretch",
     backgroundColor: "#FFFFFF",
   },
-
 });
